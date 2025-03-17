0436c84c88451539b4a5b7c46bc6bf2e1bd5290b
Rapinder <rapinder@flowell.co>
2025-03-17 12:12:10 +0530

    Changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -13,9 +14,19 @@ if (!PORT) {
   process.exit(1);
 }
 
+const _dirname = path.resolve();
+
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
